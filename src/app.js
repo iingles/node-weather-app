67320eef525e7c67d681478ecd7a7c4cd939815c
Isaac Ingles <isaac.ingles@gmail.com>
2019-12-07 17:09:58 -0700

    setting up templates

--- src/app.js
+++ src/app.js
@@ -1,29 +1,60 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+//Define paths for Express configuration
 const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+//set up the handlebars template engine
+app.set('view engine', 'hbs')
+//set our views path to templates folder location
+app.set('views', viewsPath)
+//set a path for our partial HTML stuff
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve from
 app.use(express.static(publicDirPath))
 
-app.get('/', (req, res) => {
-    res.send('Index page')
+app.get('', (req, res, next)=> {
+    //render the template in ./views
+    res.render('index', {
+        //values that I want the view to access
+        pageTitle: 'Weather App', 
+        name: 'Isaac',
+    })
 })
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
+app.get('/about', (req, res, next)=> {
+    //render the template in ./views
+    res.render('about', {
+        //values that I want the view to access
+        title: 'About', 
+        name: 'heyhey',
+    })
 })
 
-app.get('/about', (req, res) => {
-    res.send('About page')
+app.get('/help', (req, res, next)=> {
+    //render the template in ./views
+    res.render('help', {
+        //values that I want the view to access
+        title: 'help', 
+        name: 'wow',
+    })
 })
 
-app.get('weather', (req, res) => {
-    res.send('your weather')
+app.get('/form', (req, res, next)=> {
+    //render the template in ./views
+    res.render('about', {
+        //values that I want the view to access
+        title: 'form', 
+        name: 'woah',
+    })
 })
-
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
